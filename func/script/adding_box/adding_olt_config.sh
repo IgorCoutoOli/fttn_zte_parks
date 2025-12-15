44fea77d2234eaecd59abff7bd21010ff23ab63e
Igor Oliveira <igor.couto.oliveira@gmail.com>
2025-12-15 08:51:17 -0300

    Ajustado de verificações de erro e corrigido problema de args.

--- func/script/adding_box/adding_olt_config.sh
+++ func/script/adding_box/adding_olt_config.sh
@@ -6,8 +6,8 @@ set PASS [lindex $argv 2]
 set IP [lindex $argv 3]
 set GW [lindex $argv 4]
 set PORT [lindex $argv 5]
-set ALIAS [lindex $argv 5]
-set SERIE [lindex $argv 1]
+set ALIAS [lindex $argv 6]
+set SERIE [lindex $argv 7]
 set timeout 20
 
 spawn telnet $HOST
@@ -19,41 +19,38 @@ send "$USER\n"
 expect "Password:"
 send "$PASS\n"
 expect "*#"
-send "conf t\n"
-expect "*(config)#"
+send "configure terminal\n"
 expect {
     "*(config)#" { send "interface gpon$PORT\n" }
 }
 expect {
     "*ERROR" { exit 2 }
     "*Multiplexer" { exit 2 }
-    "*% Unknown command." { exit 2 }
-    "*(config-if)#"	{ send "onu prks00$SERIE ip address $IP/24 gw $GW\n" }
+    "% Unknown command." { exit 2 }
+    "*(config-if)#"     { send "onu prks00$SERIE ip address $IP/24 gw $GW\n" }
 }
 expect {
     "*ERROR" { exit 5 }
     "*Multiplexer" { exit 5 }
-    "*% Unknown command." { exit 5 }
+    "% Unknown command." { exit 5 }
     "*(config-if)#" { send "onu prks00$SERIE flow-profile 02_PROVISIONAMENTO\n" }
 }
 expect {
     "*ERROR" { exit 10 }
     "*Multiplexer" { exit 10 }
-    "*% Unknown command." { exit 10 }
+    "% Unknown command." { exit 10 }
     "*(config-if)#" { send "onu prks00$SERIE vlan-translation-profile _100 iphost\n" }
 }
 expect {
     "*ERROR" { exit 11 }
     "*Multiplexer" { exit 11 }
-    "*% Unknown command." { exit 11 }
-    "*(config-if)#"	{ send "onu prks00$SERIE alias $ALIAS\n" }
+    "% Unknown command." { exit 11 }
+    "*(config-if)#"     { send "onu prks00$SERIE alias $ALIAS\n" }
 }
 expect {
     "*ERROR" { exit 12 }
     "*Multiplexer" { exit 12 }
-    "*% Unknown command." { exit 12 }
+    "% Unknown command." { exit 12 }
     "*(config-if)#" { send "exit\n" }
 }
 expect "*#"
-send "exit\n"
-exit 0
--- func/script/adding_box/adding_profile.sh
+++ func/script/adding_box/adding_profile.sh
@@ -4,11 +4,10 @@ set HOST [lindex $argv 0]
 set USER [lindex $argv 1]
 set PASS [lindex $argv 2]
 set VLAN [lindex $argv 3]
-set PORT [lindex $argv 5]
-set FLOW [lindex $argv 6]
-set SERIE [lindex $argv 7]
+set PORT [lindex $argv 4]
+set FLOW [lindex $argv 5]
+set SERIE [lindex $argv 6]
 set timeout 20
-
 
 spawn telnet $HOST
 sleep 1
@@ -21,6 +20,9 @@ send "v3tf77n\n"
 expect "*#"
 send "config terminal\n"
 expect {
+    "*ERROR" { exit 2 }
+    "*Multiplexer" { exit 2 }
+    "*% Unknown command." { exit 2 }
     "*(config)#" { send "interface gpon$PORT\n" }
 }
 sleep 1
@@ -45,5 +47,4 @@ expect {
     "*(config-if)#" { send "exit\n" }
 }
 expect "*#"
-#send "exit\n"
-exit 0
+send "exit\n"
